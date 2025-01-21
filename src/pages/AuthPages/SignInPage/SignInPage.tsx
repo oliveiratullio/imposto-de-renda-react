dfe607b7b639f78bd631f68413df695bf4ee818a
Túllio Oliveira <105726160+oliveiratullio@users.noreply.github.com>
2025-01-21 18:58:18 -0300

    fixed imports

--- src/pages/AuthPages/SignInPage/SignInPage.tsx
+++ src/pages/AuthPages/SignInPage/SignInPage.tsx
@@ -1,55 +1,9 @@
 import axios from 'axios';
 import React, { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
 import AuthContext from '../../../contexts/AuthContext';
+import { Button, Container, FormBox, Input, Title } from './style/SignInStyle';
 
-
-const Container = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  min-height: 100vh;
-  background: #f7f7f7;
-`;
-
-const FormBox = styled.div`
-  width: 100%;
-  max-width: 400px;
-  padding: 40px;
-  background: white;
-  border-radius: 8px;
-  box-shadow: 0 4px 6px rgba(0,0,0,0.1);
-`;
-
-const Title = styled.h2`
-  text-align: center;
-  font-size: 24px;
-  font-weight: bold;
-  margin-bottom: 20px;
-`;
-
-const Input = styled.input`
-  width: 100%;
-  padding: 10px;
-  margin: 10px 0;
-  border-radius: 5px;
-  border: 1px solid #ccc;
-`;
-
-const Button = styled.button`
-  width: 100%;
-  padding: 10px;
-  background-color: #5568FE;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  margin-top: 20px;
-  cursor: pointer;
-  &:hover {
-    background-color: #4056F4;
-  }
-`;
 
 function useAuth() {
     const context = useContext(AuthContext);
@@ -59,7 +13,7 @@ function useAuth() {
     return context;
   }
 
-// Componente de Login
+
 const SignInPage = () => {
     const navigate = useNavigate();
     const { login } = useAuth();
---
+++ src/pages/AuthPages/SignInPage/style/SignInStyle.tsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  background: #f7f7f7;
+`;
+
+export const FormBox = styled.div`
+  width: 100%;
+  max-width: 400px;
+  padding: 40px;
+  background: white;
+  border-radius: 8px;
+  box-shadow: 0 4px 6px rgba(0,0,0,0.1);
+`;
+
+export const Title = styled.h2`
+  text-align: center;
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 20px;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  padding: 10px;
+  margin: 10px 0;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: #5568FE;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  margin-top: 20px;
+  cursor: pointer;
+  &:hover {
+    background-color: #4056F4;
+  }
+`;
